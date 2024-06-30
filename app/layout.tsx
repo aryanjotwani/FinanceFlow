e9e3f802659bbbff09a68f38591d27a9ad59fbda
aryanjotwani <aryanjotwani003@gmail.com>
2024-06-30 17:37:48 +0530

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { Toaster } from "@/components/ui/sonner";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Budget Tracker",
-  description: "CodeWithKliton",
+  title: "Finance Flow",
+  description: "Track your finances effortlessly with Finance Flow.",
 };
 
 export default function RootLayout({
