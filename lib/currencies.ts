a7047d8ca1fbc9c1429aba006e369ef428ea5907
aryanjotwani <aryanjotwani003@gmail.com>
2024-06-30 23:43:50 +0530

    Updated currency

--- lib/currencies.ts
+++ lib/currencies.ts
@@ -1,9 +1,12 @@
 export const Currencies = [
   { value: "USD", label: "$ Dollar", locale: "en-US" },
+  { value: "INR", label: "₹ Rupee", locale: "en-IN" },
   { value: "EUR", label: "€ Euro", locale: "de-DE" },
   { value: "JPY", label: "¥ Yen", locale: "ja-JP" },
   { value: "GBP", label: "£ Pound", locale: "en-GB" },
-  { value: "INR", label: "₹ Rupee", locale: "en-IN" },
+  { value: "CNY", label: "¥ Yuan", locale: "zh-CN" }, 
+  { value: "THB", label: "฿ Baht", locale: "th-TH" }   
 ];
+
 
 export type Currency = (typeof Currencies)[0];
